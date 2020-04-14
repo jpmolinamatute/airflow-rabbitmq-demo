1865d24cbd6b8cc612dff66319676d3815f15488
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2020-04-14 11:10:54 -0300

    working on stress testing

--- dronelogs/default_values.py
+++ dronelogs/default_values.py
@@ -1,3 +1,4 @@
+import datetime
 from airflow.hooks.base_hook import BaseHook
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 
@@ -10,13 +11,13 @@ def task_fail_slack_alert(context):
     log_url = ti.log_url
     slack_webhook_token = BaseHook.get_connection("slack").password
     slack_msg = f"""
-            :red_circle: Task Failed.
-            ----------------
-            *Task*: {task_id}
-            *Dag*: {dag_id}
-            *Execution Time*: {exec_date}
-            *Log Url*: {log_url}
-            """
+        :red_circle: Task Failed.
+        ----------------
+        *Task*: {task_id}
+        *Dag*: {dag_id}
+        *Execution Time*: {exec_date}
+        *Log Url*: {log_url}
+    """
     failed_alert = SlackWebhookOperator(
         task_id="slack_test",
         http_conn_id="slack",
@@ -36,19 +37,21 @@ def task_successed_slack_alert(context):
     log_url = ti.log_url
     slack_webhook_token = BaseHook.get_connection("slack").password
     slack_msg = f"""
-            Task Successed.
-            ---------------
-            *Task*: {task_id}
-            *Dag*: {dag_id}
-            *Execution Time*: {exec_date}
-            *Log Url*: {log_url}
-            """
+        Task Successed.
+        ---------------
+        *Task*: {task_id}
+        *Dag*: {dag_id}
+        *Execution Time*: {exec_date}
+        *Log Url*: {log_url}
+    """
     failed_alert = SlackWebhookOperator(
         task_id="slack_test",
         http_conn_id="slack",
         webhook_token=slack_webhook_token,
         message=slack_msg,
         username="airflow",
+        retries=2,
+        retry_delay=datetime.timedelta(seconds=2),
     )
 
     return failed_alert.execute(context=context)
@@ -58,5 +61,5 @@ DEFAULT_VALUES = {
     "owner": "airflow",
     "depends_on_past": False,
     "on_failure_callback": task_fail_slack_alert,
-    "on_success_callback": task_successed_slack_alert,
+    # "on_success_callback": task_successed_slack_alert,
 }
