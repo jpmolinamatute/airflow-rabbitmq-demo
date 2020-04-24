be5a1be02ebabe0bd345f0935817e2d903e013bd
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2020-04-24 20:08:10 -0300

    Working on new index system

--- dronelogs/index/run.py
+++ dronelogs/index/run.py
@@ -8,12 +8,13 @@ import boto3
 CONN = boto3.client("s3")
 
 
-def create_no_lines_file(no_lines):
+def create_no_lines_file(file_prefix, no_lines):
     with open("./no_lines.txt", "w") as file_obj:
         file_obj.write(str(no_lines))
+    upload_file(environ["AWS_BUCKET_NAME"], f"{file_prefix}/no_lines.txt", "./no_lines.txt")
 
 
-def create_index(index_file_name):
+def init(file_prefix, file_name):
     bucket = environ["AWS_RAW_S3_BUCKET"]
     prefix = environ["AWS_RAW_S3_PREFIX"]
     objs = CONN.list_objects_v2(Bucket=bucket, Prefix=prefix)
@@ -22,11 +23,18 @@ def create_index(index_file_name):
 
     no_lines = 0
     keep_going = True
-    days_file = open(f"./{index_file_name}", "w+")
+    index_file = open(f"./index.txt", "w+")
     while keep_going:
+        sub_index_file = open("./sub-index.txt", "w+")
+        start_point = no_lines + 1
         for name in objs["Contents"]:
             no_lines += 1
-            days_file.write(f'{name["Key"]}\n')
+            sub_index_file.write(f'{name["Key"]}\n')
+        sub_index_file.close()
+        upload_file_name = f"{file_prefix}/sub-index-{str(start_point)}-{str(no_lines)}"
+        upload_file(environ["AWS_BUCKET_NAME"], upload_file_name, "./sub-index.txt")
+        index_file.write(f"{upload_file_name}\n")
+
         if "NextContinuationToken" in objs:
             objs = CONN.list_objects_v2(
                 Bucket=bucket, Prefix=prefix, ContinuationToken=objs["NextContinuationToken"]
@@ -34,17 +42,9 @@ def create_index(index_file_name):
 
         else:
             keep_going = False
-    days_file.close()
-    return no_lines
-
-
-def init(file_prefix, file_name):
-    # CONN.delete_object(Bucket=environ["AWS_BUCKET_NAME"], Key=f"{file_prefix}/{file_name}")
-    no_lines = create_index(file_name)
-    if no_lines > 0:
-        create_no_lines_file(no_lines)
-        upload_file(environ["AWS_BUCKET_NAME"], f"{file_prefix}/{file_name}", f"./{file_name}")
-        upload_file(environ["AWS_BUCKET_NAME"], f"{file_prefix}/no_lines.txt", "./no_lines.txt")
+    index_file.close()
+    create_no_lines_file(file_prefix, no_lines)
+    upload_file(environ["AWS_BUCKET_NAME"], f"{file_prefix}/index.txt", "./index.txt")
 
 
 if __name__ == "__main__":
